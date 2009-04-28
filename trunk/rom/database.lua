713ee8a26aa3f0db09239d633dfec47c3de2fa87
mystikfox <mystikfox@ca78ecfe-2e4a-11de-874f-bb9810f3bc79>
2009-04-28 20:49:53 +0000

    Fixed priority/skill type loading in database.lua

--- trunk/rom/database.lua
+++ trunk/rom/database.lua
@@ -35,11 +35,43 @@ function database.load()
 			cooldown = 1;
 		end
 
+		--[[
 		if( not priority and type == "damage" ) then type = STYPE_DAMAGE; priority = 70; end;
 		if( not priority and type == "hot" ) then type = STYPE_HOT; priority = 110; end;
 		if( not priority and type == "heal" ) then type = STYPE_HEAL; priority = 100; end;
 		if( not priority and type == "buff" ) then type = STYPE_BUFF; priority = 90; end;
 		if( not priority and type == "dot" ) then type = STYPE_DOT; priority = 80; end;
+		]]
+
+		-- Automatically assign priority (if not given) based on type
+		if( not priority ) then
+			if( type == "damage" ) then
+				priority = 70;
+			elseif( type == "hot" ) then
+				priority = 110;
+			elseif( type == "heal" ) then
+				priority = 100;
+			elseif( type == "buff" ) then
+				priority = 90;
+			elseif( type == "dot" ) then
+				priority = 80;
+			end;
+		end
+
+		-- Re-assign type to an actual defined type (improves speed; reduces slow string compares)
+		if( type == "damage" ) then
+			type = STYPE_DAMAGE;
+		elseif( type == "hot" ) then
+			type = STYPE_HOT;
+		elseif( type == "heal" ) then
+			type = STYPE_HEAL;
+		elseif( type == "buff" ) then
+			type = STYPE_BUFF;
+		elseif( type == "dot" ) then
+			type = STYPE_DOT;
+		end;
+
+
 
 		if( target == "enemy" ) then target = STARGET_ENEMY; end;
 		if( target == "self" ) then target = STARGET_SELF; end;
