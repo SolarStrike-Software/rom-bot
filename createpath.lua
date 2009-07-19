97c1690f40bc7f979b1f52527cfb869c1fdf4f20
d003232 <d003232@ca78ecfe-2e4a-11de-874f-bb9810f3bc79>
2009-07-19 23:54:36 +0000

    createpath.lua: press 2 for harvest point (insert harvest command)

--- createpath.lua
+++ createpath.lua
@@ -3,6 +3,23 @@ include("classes/player.lua");
 include("classes/waypoint.lua");
 include("functions.lua");
 
+-- ********************************************************************
+-- Change the parameters below to your need                           *
+-- ********************************************************************
+-- if you want to create waypoint files with special waypoint types
+-- like type=TRAVEL, than you can change the global variables
+-- below to your need, see the following example
+-- p_wp_gtype = " type=\"TRAVEL\"";	-- global type for whole file
+-- p_wp_type = " type=\"TRAVEL\"";	-- type for normal waypoints 
+-- p_hp_type = " type=\"TRAVEL\"";	-- type for harvest waypoints		
+p_wp_gtype = "";	-- global type for whole file: e.g. TRAVEL
+p_wp_type = "";		-- type for normal waypoints 
+p_hp_type = "";		-- type for harvest waypoints		
+p_harvest_command = "player:harvest();";	-- harvest command
+-- ********************************************************************
+-- End of Change parameter changes                                    *
+-- ********************************************************************
+
 if( getVersion() < 100 ) then
 	startKey = key.VK_DELETE;
 	stopKey = key.VK_END;
@@ -11,8 +28,9 @@ else
 	setStopKey(key.VK_END);
 end
 
-wpKey = key.VK_NUMPAD1; -- insert a movement point
-saveKey = key.VK_NUMPAD3; -- save the waypoints
+wpKey = key.VK_NUMPAD1;		-- insert a movement point
+harvKey = key.VK_NUMPAD2;	-- insert a harvest point	
+saveKey = key.VK_NUMPAD3;	-- save the waypoints
 
 function saveWaypoints(list)
 	keyboardBufferClear();
@@ -25,9 +43,20 @@ function saveWaypoints(list)
 		error(err, 0);
 	end
 
-	file:write("<waypoints>\n");
+	local str = sprintf("<waypoints%s>\n", p_wp_gtype);	-- create first tag
+	file:write(str);					-- write first tag
+
 	for i,v in pairs(list) do
-		local str = sprintf("\t<!-- #%2d --><waypoint x=\"%d\" z=\"%d\"></waypoint>\n", i, v.X, v.Z);
+
+		if( v.harvPoint == true ) then 		-- it's a harvest point?
+			hf_temp1 = p_hp_type;	-- insert type=TRAVEL for harvest points if you want
+			hf_temp2 = p_harvest_command;	-- then insert harvest command
+		else 
+			hf_temp1 = p_wp_type;		-- normal waypoint type
+			hf_temp2 = ""; 			-- no special command
+		end;							
+
+		local str = sprintf("\t<!-- #%2d --><waypoint x=\"%d\" z=\"%d\"%s>%s</waypoint>\n", i, v.X, v.Z, hf_temp1, hf_temp2);
 		file:write(str);
 	end
 	file:write("</waypoints>\n");
@@ -44,28 +73,62 @@ function main()
 
 	cprintf(cli.green, "RoM waypoint creator\n");
 	printf("Hotkeys:\n  (%s)\tInsert new waypoint(at player position)\n"
+		.. "  (%s)\tInsert new harvest waypoint(at player position)\n"	
 		.. "  (%s)\tSave waypoints\n",
-		getKeyName(wpKey), getKeyName(saveKey));
+		getKeyName(wpKey), getKeyName(harvKey), getKeyName(saveKey));
 
-	local lastwpKey = false;
 	while(true) do
-		if( keyPressed(wpKey) == false and lastwpKey == true ) then
-			player:update();
-			local tmp = CWaypoint(player.X, player.Z);
-			printf("Recorded [#%2d] Continue to the next. Press %s to save and quit\n",
-				#wpList + 1, getKeyName(saveKey));
-			table.insert(wpList, tmp);
-		end
-		if( keyPressed(wpKey) ) then
-			lastwpKey = true;
-		else
-			lastwpKey = false;
-		end
 
-		if( keyPressed(saveKey) ) then
-			saveWaypoints(wpList);
-			break;
-		end
+		hf_key_pressed = false;
+
+		if( keyPressed(wpKey) ) then	-- normal waypoint key pressed
+			hf_key_pressed = true;
+			hf_key = "WP";
+		end;
+		if( keyPressed(harvKey) ) then	-- harvest waypoint key pressed
+			hf_key_pressed = true;
+			hf_key = "HP";
+		end;
+		if( keyPressed(saveKey) ) then	-- save key pressed
+			hf_key_pressed = true;
+			hf_key = "SAVE";
+		end;
+
+		if( hf_key_pressed == false ) then	-- key released, do the work
+
+			-- SAVE Key: save waypoint file and exit
+			if( hf_key == "SAVE" ) then
+				saveWaypoints(wpList);
+				break;
+			end;
+
+			-- waypoint or harvest point key: create a waypoint/harvest waypoint
+			if( hf_key == "WP"  or		-- normal waypoint
+			    hf_key == "HP"  ) then	-- harvet waypoint
+
+				player:update();
+
+				local tmp = {};		
+				tmp.X = player.X;		
+				tmp.Z = player.Z;		
+
+				if( hf_key == "HP" ) then 	-- is's a havest point?
+					tmp.harvPoint = true;	-- it is a harvest point
+					hf_temp = "HP";
+				else 
+					tmp.harvPoint = false; 
+					hf_temp = "WP";
+				end; 
+
+				printf("Recorded [#%2d] %s, Continue to next. Press %s to save and quit\n",
+				#wpList + 1, hf_temp,getKeyName(saveKey));
+
+				table.insert(wpList, tmp);
+
+			end;
+
+			hf_key = " ";	-- clear last pressed key
+		end;
 
 		yrest(10);
 	end
