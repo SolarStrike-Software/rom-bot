affbb69180eec373acebf581af623dfd2fa8fc42
d003232 <d003232@ca78ecfe-2e4a-11de-874f-bb9810f3bc79>
2009-07-23 23:45:08 +0000

    Added date/time to logout message

--- language/english.lua
+++ language/english.lua
@@ -33,6 +33,6 @@ language = {
 	[40] = "Player address changed: 0x%X\n",
 	[41] = "Failed to read memory",
 
-	[50] = "Auto-logging out.\n",
+	[50] = "%s  Auto-logging out.\n",
 	[51] = "Shutting system down.\n",
 };
