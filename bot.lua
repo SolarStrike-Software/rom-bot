0f35aa42a3d36d3c352e2f0237ec0f2328a30f1e
mystikfox <mystikfox@ca78ecfe-2e4a-11de-874f-bb9810f3bc79>
2009-09-14 04:52:29 +0000

    Fixed: Reverted keyboardSetDelay() change until a fix for the bug it exposes can be made

--- bot.lua
+++ bot.lua
@@ -68,7 +68,7 @@ function main()
 
 	database.load();
 
-	keyboardSetDelay(0); -- TEMP FIX! Remove after MicroMacro 7 beta full release
+	--keyboardSetDelay(0); -- TEMP FIX! Remove after MicroMacro 7 beta full release
 	attach(getWin());
 
 	if( not checkExecutableCompatible() ) then
