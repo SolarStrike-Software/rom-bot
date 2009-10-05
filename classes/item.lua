53c9d6b8b1b409ce8c8f11cbaea8c5769d347d73
d003232 <d003232@ca78ecfe-2e4a-11de-874f-bb9810f3bc79>
2009-10-05 11:44:07 +0000

    Fixed: refix the skiping of nil bagIds

--- classes/item.lua
+++ classes/item.lua
@@ -57,17 +57,13 @@ function CItem:update()
 
 -- FIX: THERE SEEM TO BE A BUG IN THE ROM CLIENT COMMUNICATION
 -- in very rar cases, the client deliver an empty or wrong bagId
--- could be the client or the RoMScript
+-- e.g. if we press a modifier while running the bot in background
 -- sometimes slot 1-10 don't have bagid 61-70? I don't know the rule :-(
 -- so we can only check missing bagIds, but not wrong bagIds
-
--- UNFIX: this fix breaks the bestAvailableConsumable function, because item HAS TO HAVE default values.
-
---	if( bagId == "" or bagId == nil) then
---		cprintf(cli.yellow, "Item:update(): empty bagid return, we don't update slot %s name %s\n", self.SlotNumber, self.Name);
---		return;		-- dont' change the values, the new ones are wrong
---		player.InventoryDoUpdate = true;
---	end
+	if( bagId == "" or bagId == nil) then
+		cprintf(cli.yellow, "Item:update(): empty bagid return, we don't update slot %s name %s\n", self.SlotNumber, self.Name);
+		return;		-- dont' change the values, the new ones are wrong
+	end
 
 	if (itemLink == "") then		-- no item in slot
 		self = CItem(self.SlotNumber);
