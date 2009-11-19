aabc9b50a2a39c035048e25a7ac1809c91c8a53d
mystikfox <mystikfox@ca78ecfe-2e4a-11de-874f-bb9810f3bc79>
2009-11-19 16:36:15 +0000

    Fixed: Do not assume that the player information will be readable while updating the game (function selectGame()). Fixed: New addresses.lua Changed: Now version 3.10

--- addresses.lua
+++ addresses.lua
@@ -35,8 +35,8 @@ addresses = {
 	pawnX_offset = 0x2C,
 	pawnY_offset = 0x30,
 	pawnZ_offset = 0x34,
-	staticbase_char = 0x8FFC10,
-	staticbase_macro = 0x94DF08,
-	staticpattern_char = 0x5D703D,
-	staticpattern_macro = 0x70C5EF,
+	staticbase_char = 0x901990,
+	staticbase_macro = 0x94FF00,
+	staticpattern_char = 0x5D7C2D,
+	staticpattern_macro = 0x70A6FF,
 }
