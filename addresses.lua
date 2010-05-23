45fea73452b9e94979324ca4fed925737d69fd05
mystikfox <mystikfox@ca78ecfe-2e4a-11de-874f-bb9810f3bc79>
2010-05-23 19:14:56 +0000

    Fix: New addresses.lua Fix: Include information about Moa mounts

--- addresses.lua
+++ addresses.lua
@@ -37,8 +37,8 @@ addresses = {
 	pawnX_offset = 0x28,
 	pawnY_offset = 0x2C,
 	pawnZ_offset = 0x30,
-	staticbase_char = 0x995F3C,
-	staticbase_macro = 0x9E9704,
-	staticpattern_char = 0x5DAD4D,
-	staticpattern_macro = 0x7406A5,
+	staticbase_char = 0x9A116C,
+	staticbase_macro = 0x9F397C,
+	staticpattern_char = 0x5DB60D,
+	staticpattern_macro = 0x7422C5,
 }
