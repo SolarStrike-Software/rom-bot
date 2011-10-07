2a6b2d251c60486301219b280caf93b390cbc1f5
lconners90@yahoo.com.au <lconners90@yahoo.com.au@ca78ecfe-2e4a-11de-874f-bb9810f3bc79>
2011-10-07 01:15:20 +0000

    redid the find party names with in game macros. Only redoes table of names every 60 seconds to reduce load. This is only a temp fix until we can refind the missing address.

--- trunk/rom/functions.lua
+++ trunk/rom/functions.lua
@@ -1266,8 +1266,17 @@ end
 --== addresses moved to addresses.lua ==--
 
 function GetPartyMemberName(_number)
-
-	if type(_number) ~= "number" or _number < 1 then
+	if partymembers == nil or (os.time() - _lastupdated) >= 60 then
+		nummembers = sendMacro("GetNumPartyMembers()")
+		partymembers={} 
+		for i = 1, nummembers do
+			_name = sendMacro("GetPartyMember("..i..")")
+			table.insert(partymembers,i, _name)
+			_lastupdated = os.time()
+		end
+	end
+	return partymembers[_number]
+	--[[if type(_number) ~= "number" or _number < 1 then
 		print("GetPartyMemberName(number): incorrect value for 'number'.")
 		return
 	end
@@ -1281,12 +1290,12 @@ function GetPartyMemberName(_number)
 		return nil
 	end
 
-	return memoryReadRepeat("string", getProc(), memberAddress + 8 )
+	return memoryReadRepeat("string", getProc(), memberAddress + 8 )]]
 end
 
 function GetPartyMemberAddress(_number)
 
-	if type(_number) ~= "number" or _number < 1 then
+	--[[if type(_number) ~= "number" or _number < 1 then
 		print("GetPartyMemberName(number): incorrect value for 'number'.")
 		return
 	end
@@ -1300,7 +1309,8 @@ function GetPartyMemberAddress(_number)
 		return
 	end
 		local memberName = memoryReadRepeat("string", getProc(), memberAddress + 8 )
-	return player:findNearestNameOrId(memberName)
+	return player:findNearestNameOrId(memberName)]]
+	return player:findNearestNameOrId(GetPartyMemberName(_number))
 end
 
 function EventMonitorStart(monitorName, event, filter)
