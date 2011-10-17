20ac328c1a9105cfa747e37c55de9c7ba254da3a
lconners90@yahoo.com.au <lconners90@yahoo.com.au@ca78ecfe-2e4a-11de-874f-bb9810f3bc79>
2011-10-17 04:07:09 +0000

    Fixed an issue in functions.lua

--- functions.lua
+++ functions.lua
@@ -1458,7 +1458,7 @@ function Attack()
 			end
 
 			-- freeze TargetPtr
-			memoryWriteString(getProc(), functionTargetPatchAddr, string.char(0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90));
+			memoryWriteString(getProc(), addresses.functionTargetPatchAddr, string.char(0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90));
 
 			-- Target it
 			memoryWriteInt(getProc(), player.Address + addresses.pawnTargetPtr_offset, player.TargetPtr);
@@ -1472,7 +1472,7 @@ function Attack()
 			yrest(100)
 
 			-- unfreeze TargetPtr
-			memoryWriteString(getProc(), functionTargetPatchAddr, string.char(0x56, 0x8B, 0xCD, 0xE8, 0x99, 0x95, 0x27, 0x00));
+			memoryWriteString(getProc(), addresses.functionTargetPatchAddr, string.char(0x56, 0x8B, 0xCD, 0xE8, 0x99, 0x95, 0x27, 0x00));
 
 		end
 	end
