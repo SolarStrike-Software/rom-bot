024f01bd2e3ba90367e6485772d793ec1240a41b
Katherine Sterling <katherine@oceanexecutive.com>
2020-04-18 15:51:13 -0500

    Skill levelcheck fix

--- settings.lua
+++ settings.lua
@@ -1367,8 +1367,8 @@ function settings.updateSkillsAvailability()
 				if tabData[realName].skillnum then database.skills[skill.Name].skillnum = tabData[realName].skillnum end
 
 				-- Check if available
-				if skill.skilltab == 3 then
-					if skill.aslevel > player.Level2  then
+				if skill.skilltab > 3 then
+					if skill.aslevel > player.Level2 then
 						skill.Available = false
 					else
 						skill.Available = true
