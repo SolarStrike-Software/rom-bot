81b33155b46759a500d1536e8c093c06c3434c60
Katherine Sterling <katherine@oceanexecutive.com>
2020-05-23 09:56:38 -0500

    Improve deprecated update script warning

--- update.lua
+++ update.lua
@@ -1,1 +1,12 @@
-error("Update script is deprecated\nUse rom-bot/addrupdate instead.");
+local path = getExecutionPath();
+local dir = getFilePath(path .. "/../");
+
+if( string.sub(path, 1, #dir) == dir ) then
+	local loc = #dir + 1;
+	if( string.sub(path, loc, loc) == "/" ) then
+		loc = loc + 1;
+	end
+	path = string.sub(path, loc);
+end
+
+error(sprintf("Update script is deprecated\nUse %s/addrupdate instead.", path));
