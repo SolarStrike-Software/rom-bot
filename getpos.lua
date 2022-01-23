cd4bae6cd23daf7238f4858d6bd0164a30069935
elverion <admin@solarstrike.net>
2022-01-23 19:24:31 -0400

    getpos.lua updated to use new(er) addresses.

--- getpos.lua
+++ getpos.lua
@@ -2,6 +2,7 @@
 
 include("addresses.lua");
 include("functions.lua");
+include("classes/player.lua");
 
 setStartKey(0);
 setStopKey(0);
@@ -24,22 +25,23 @@ function main()
 	local playerZ = 0
 	local playerY = 0
 
+	local gameroot = getBaseAddress(addresses.game_root.base);
 	while(true) do
 		yrest(500);
-		playerAddress = memoryReadUIntPtr(getProc(), addresses.staticbase_char, addresses.charPtr_offset);
-		playerId = memoryReadInt(getProc(), playerAddress + addresses.pawnId_offset) or 0
-		playerHP = memoryReadInt(getProc(), playerAddress + addresses.pawnHP_offset) or 0
+		playerAddress = memoryReadRepeat("uintptr", getProc(), gameroot, addresses.game_root.player.base);
+		playerId = memoryReadInt(getProc(), playerAddress + addresses.game_root.pawn.id) or 0
+		playerHP = memoryReadInt(getProc(), playerAddress + addresses.game_root.pawn.hp) or 0
 		if not isInGame() or playerId < PLAYERID_MIN or playerId > PLAYERID_MAX or playerHP < 1 then
 			repeat
 				yrest(1000)
-				playerAddress = memoryReadUIntPtr(getProc(), addresses.staticbase_char, addresses.charPtr_offset);
-				playerId = memoryReadInt(getProc(), playerAddress + addresses.pawnId_offset) or 0
-				playerHP = memoryReadInt(getProc(), playerAddress + addresses.pawnHP_offset) or 0
+				playerAddress = memoryReadRepeat("uintptr", getProc(), gameroot, addresses.game_root.player.base);
+				playerId = memoryReadInt(getProc(), playerAddress + addresses.game_root.pawn.id) or 0
+				playerHP = memoryReadInt(getProc(), playerAddress + addresses.game_root.pawn.hp) or 0
 			until isInGame() and playerId >= PLAYERID_MIN and playerId <= PLAYERID_MAX and playerHP > 1
 		end
-		playerX = memoryReadFloat(getProc(), playerAddress + addresses.pawnX_offset) or playerX
-		playerY = memoryReadFloat(getProc(), playerAddress + addresses.pawnY_offset) or playerY
-		playerZ = memoryReadFloat(getProc(), playerAddress + addresses.pawnZ_offset) or playerZ
+		playerX = memoryReadFloat(getProc(), playerAddress + addresses.game_root.pawn.x) or playerX
+		playerY = memoryReadFloat(getProc(), playerAddress + addresses.game_root.pawn.y) or playerY
+		playerZ = memoryReadFloat(getProc(), playerAddress + addresses.game_root.pawn.z) or playerZ
 
 		printf("\rPosition: (%d, %d, %d)\t", playerX, playerZ, playerY);
 	end
